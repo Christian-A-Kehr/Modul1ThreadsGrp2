bcffc76bf7dab74dd865a6d6a670bdc28d527ba5
MukHansen <Fubar93@gmail.com>
2019-02-06 13:53:14 +0100

    Opgave3_gul original oprettet

--- src/main/java/dat/sem2/threads/opgaver/Opgave3_rød.java
+++ src/main/java/dat/sem2/threads/opgaver/Opgave3_rød.java
@@ -15,55 +15,51 @@ import java.util.concurrent.Executors;
  * @author Mads Egevang Jensen
  */
 public class Opgave3_rød {
-    
-    public static void main( String[] args ) {
-        ExecutorService workingJack = Executors.newFixedThreadPool( 17 );
-        MyTask4.init();
-        MyTask4.showList();
-        System.out.println( "Main starts" );
-        for ( int count = 0; count < 100; count++ ) {
-            Runnable task = new MyTask4( count );
-            workingJack.submit( task );
+
+    public static void main(String[] args) {
+        ExecutorService workingJack = Executors.newFixedThreadPool(17);
+        MyTask5.init();
+        MyTask5.showList();
+        System.out.println("Main starts");
+        for (int count = 0; count < 100; count++) {
+            Runnable task = new MyTask5(count);
+            workingJack.submit(task);
         }
-        System.out.println( "Main is done" );
+        System.out.println("Main is done");
         workingJack.shutdown();
-        MyTask4.showList();
+        MyTask5.showList();
 
     }
 
 }
 
-class MyTask4 implements Runnable {
+class MyTask5 implements Runnable {
 
     private int count = 0;
     private int sleepTime = 0;
 
     final private static List<Integer> list = new ArrayList<>();
 
-    public static void init()
-    {
-        for(int i = 0; i < 100; ++i)
-        {
+    public static void init() {
+        for (int i = 0; i < 100; ++i) {
             list.add(-1);
         }
     }
-    
-    public static void showList()
-    {
-        for(int i : list)
-        {
+
+    public static void showList() {
+        for (int i : list) {
             System.out.println("List element: " + i);
         }
     }
-    
-    MyTask4( int cnt ) {
-        sleepTime = (int) ( Math.random() * 800 + 200 ); // At least 200 ms, up to one sec
+
+    MyTask5(int cnt) {
+        sleepTime = (int) (Math.random() * 800 + 200); // At least 200 ms, up to one sec
         count = cnt;
     }
 
     @Override
     public void run() {
-        list.set( count, count );
-        System.out.println( "Task: " + count + ": List size = " + list.size() );
+        list.set(count, count);
+        System.out.println("Task: " + count + ": List size = " + list.size());
     }
 }
